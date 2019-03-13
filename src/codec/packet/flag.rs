6c8d6e9c089b555c27c05bf8dd8a65413171f999
Daniel Santana <danielgsantana@gmail.com>
2019-03-13 11:07:55 +0000

    Fix packet flags.

--- src/codec/packet/flag.rs
+++ src/codec/packet/flag.rs
@@ -3,7 +3,7 @@ use libc::c_int;
 
 bitflags! {
     pub struct Flags: c_int {
-        const KEY     = AV_PKT_FLAG_KEY;
-        const CORRUPT = AV_PKT_FLAG_CORRUPT;
+        pub const KEY     = AV_PKT_FLAG_KEY;
+        pub const CORRUPT = AV_PKT_FLAG_CORRUPT;
     }
 }
