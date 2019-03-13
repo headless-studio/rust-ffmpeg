7512cba26ad239ea5618629202b3479b6a712d1d
Daniel Santana <danielgsantana@gmail.com>
2019-03-13 11:09:07 +0000

    Reverted

--- src/codec/packet/flag.rs
+++ src/codec/packet/flag.rs
@@ -3,7 +3,7 @@ use libc::c_int;
 
 bitflags! {
     pub struct Flags: c_int {
-        pub const KEY     = AV_PKT_FLAG_KEY;
-        pub const CORRUPT = AV_PKT_FLAG_CORRUPT;
+        const KEY     = AV_PKT_FLAG_KEY;
+        const CORRUPT = AV_PKT_FLAG_CORRUPT;
     }
 }
